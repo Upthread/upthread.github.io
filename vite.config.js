22473dd65e0159af9cb1704bc2f4cc95e79f7de3
tactnician <kudzu@protonmail.com>
2022-09-08 21:34:27 -0400

    added public path to base url

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    },
+    base: process.env.NODE_ENV === "production" ? "/upthread.github.io/" : "/"
   }
 })
